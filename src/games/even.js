cb990c813ec40f108cc0c7e4ac725333f381d00f
Anna Kotova <kotovanna2001@gmail.com>
2025-01-05 17:05:50 +0300

    isEven fix

--- src/games/even.js
+++ src/games/even.js
@@ -3,7 +3,7 @@ import getRandomInt from '../helpers/getRandomInt.js';
 export default () => {
   const gameCondition = 'Answer "yes" if the number is even, otherwise answer "no".';
   const randomInt = getRandomInt();
-  const rightAnswer = getRandomInt() % 2 === 0 ? 'yes' : 'no';
+  const rightAnswer = randomInt % 2 === 0 ? 'yes' : 'no';
   const gameTask = `${randomInt}`;
   return { gameCondition, gameTask, rightAnswer };
 };
